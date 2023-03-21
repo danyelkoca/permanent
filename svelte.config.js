e61e915e97ab269f7d9f4c002c51b53ed999d0b3
Danyel Koca <danyelkoca@gmail.com>
2023-03-21 18:14:42 +0900

    yes

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,14 @@
 import adapter from '@sveltejs/adapter-static';
 
+const dev = process.argv.includes('dev');
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: null
-		})
+		adapter: adapter({strict:false}),
+		paths: {
+			base: dev ? '' : '/permanent'
+		},
 	}
 };
 
